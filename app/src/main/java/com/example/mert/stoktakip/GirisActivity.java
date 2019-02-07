ead458ff8aa99f2c915764ffc13043eafae1be98
mertcansegmen <mertcan.segmen@hotmail.com>
2019-02-08 01:16:08 +0300

    Giriş yapma fonksiyonları tamamlandı.

--- app/src/main/java/com/example/mert/stoktakip/GirisActivity.java
+++ app/src/main/java/com/example/mert/stoktakip/GirisActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GirisActivity extends AppCompatActivity {
 
@@ -29,10 +30,39 @@ public class GirisActivity extends AppCompatActivity {
     }
 
     private void girisYap() {
+        Kullanici kullanici = new Kullanici();
+        kullanici.setKadi(kadiTxt.getText().toString());
+        kullanici.setSifre(sifreTxt.getText().toString());
+
+        VeritabaniYonetici vy = new VeritabaniYonetici(GirisActivity.this);
+
+        // Alanlardan herhangi biri boşsa hata ver.
+        if(kullanici.getKadi().equals("") || kullanici.getSifre().equals("")){
+            Toast.makeText(GirisActivity.this, "Lütfen bütün alanları doldurun.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        // Eğer kullanıcı adı ya da şifre yanlışsa hata ver.
+        if(!vy.kullaniciKontrolEt(kullanici.getKadi(), kullanici.getSifre())){
+            Toast.makeText(GirisActivity.this, "Kullanıcı adı ya da şifre yanlış.",
+                    Toast.LENGTH_LONG).show();
+            sifreTxt.setText(null);
+            return;
+        }
+        // Giriş yapıldıysa anasayfaya yönlendir.
+        alanlariBosalt();
+        Intent intent = new Intent(GirisActivity.this, AnasayfaActivity.class);
+        intent.putExtra("kadi", kullanici.getKadi());
+        startActivity(intent);
     }
 
     private void uyeOl(){
         Intent intent = new Intent(GirisActivity.this, UyeOlActivity.class);
         startActivity(intent);
+    }
+
+    private void alanlariBosalt(){
+        sifreTxt.setText(null);
+        kadiTxt.setText(null);
     }
 }
