d82d25d115d727f53e3bc9b70843c9ec217ee74a
mertcansegmen <mertcan.segmen@hotmail.com>
2019-03-01 21:19:01 +0300

    Fragmentlar eklendi

--- app/src/main/java/com/example/mert/stoktakip/AnasayfaActivity.java
+++ app/src/main/java/com/example/mert/stoktakip/AnasayfaActivity.java
@@ -1,9 +1,6 @@
 package com.example.mert.stoktakip;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AnasayfaActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,6 +27,9 @@ public class AnasayfaActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        getSupportActionBar().setTitle(R.string.nav_stoklistesi_title);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StokListesiFragment()).commit();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -73,23 +74,31 @@ public class AnasayfaActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_stoklistesi) {
-
+            getSupportActionBar().setTitle(R.string.nav_stoklistesi_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StokListesiFragment()).commit();
         } else if (id == R.id.nav_urunsat) {
-
+            getSupportActionBar().setTitle(R.string.nav_urunsat_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UrunSatFragment()).commit();
         } else if (id == R.id.nav_urunal) {
-
+            getSupportActionBar().setTitle(R.string.nav_urunal_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UrunAlFragment()).commit();
         } else if (id == R.id.nav_azalanurunler) {
-
+            getSupportActionBar().setTitle(R.string.nav_azalanurunler_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AzalanUrunlerFragment()).commit();
         } else if (id == R.id.nav_islemgecmisi) {
-
+            getSupportActionBar().setTitle(R.string.nav_islemgecmisi_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IslemGecmisiFragment()).commit();
         } else if (id == R.id.nav_istatistikler) {
-
+            getSupportActionBar().setTitle(R.string.nav_istatistikler_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IstatistiklerFragment()).commit();
         } else if (id == R.id.nav_veritabani) {
-
+            getSupportActionBar().setTitle(R.string.nav_veritabani_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new VeritabaniFragment()).commit();
         } else if (id == R.id.nav_ayarlar) {
-
+            getSupportActionBar().setTitle(R.string.nav_ayarlar_title);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AyarlarFragment()).commit();
         } else if (id == R.id.nav_uygulamayipuanla) {
-
+            Toast.makeText(this, "Uygulamanın Google Play sayfasına yönlendiriliyorsunuz...", Toast.LENGTH_LONG).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
