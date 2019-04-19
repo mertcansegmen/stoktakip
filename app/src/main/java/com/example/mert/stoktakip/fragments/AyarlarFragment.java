079dc8e6b86ced6c5605f25622bff07b17f7d7f7
mertcansegmen <mertcan.segmen@hotmail.com>
2019-04-19 03:10:45 +0300

    AyarlarFragment ve AzalanUrunlerFragment tasarlandı, azalan ürün eşiği için SharedPreferences yapısı oluşturuldu

--- app/src/main/java/com/example/mert/stoktakip/fragments/AyarlarFragment.java
+++ app/src/main/java/com/example/mert/stoktakip/fragments/AyarlarFragment.java
@@ -1,20 +1,57 @@
 package com.example.mert.stoktakip.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mert.stoktakip.R;
+import com.jeevandeshmukh.glidetoastlib.GlideToast;
 
 public class AyarlarFragment extends Fragment {
+
+    EditText esikTV;
+    Button kaydetBtn;
+
+    int varsayilanEsik = 10;
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ayarlar, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_ayarlar, container, false);
+
+        esikTV = v.findViewById(R.id.txt_esik);
+        kaydetBtn = v.findViewById(R.id.btn_kaydet);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        editor = preferences.edit();
+
+        esikTV.setText(Integer.toString(preferences.getInt("esik", varsayilanEsik)));
+
+        kaydetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int esik = Integer.valueOf(esikTV.getText().toString());
+                if (esik != preferences.getInt("esik", varsayilanEsik)){
+                    editor.putInt("esik", esik);
+                    editor.apply();
+                }
+                new GlideToast.makeToast(getActivity(), "Ayarlar güncellendi.",
+                        GlideToast.LENGTHTOOLONG, GlideToast.SUCCESSTOAST).show();
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/example/mert/stoktakip/fragments/AzalanUrunlerFragment.java
+++ app/src/main/java/com/example/mert/stoktakip/fragments/AzalanUrunlerFragment.java
@@ -1,6 +1,9 @@
 package com.example.mert.stoktakip.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,6 +26,7 @@ public class AzalanUrunlerFragment extends Fragment {
     UrunAdapterStokListesi adapter;
     ArrayList<Urun> urunler;
     VeritabaniIslemleri vti;
+    int varsayilanEsik = 10;
     int esik;
 
     @Nullable
@@ -32,11 +36,18 @@ public class AzalanUrunlerFragment extends Fragment {
 
         liste = v.findViewById(R.id.list_urun);
 
+        preferencesKontrol();
+
         vti = new VeritabaniIslemleri(getContext());
-        urunler = vti.azalanUrunGetir(10);
+        urunler = vti.azalanUrunGetir(esik);
         adapter = new UrunAdapterStokListesi(getContext(),R.layout.liste_elemani_stok_listesi, urunler);
         liste.setAdapter(adapter);
 
         return v;
+    }
+
+    public void preferencesKontrol(){
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        esik = pref.getInt("esik", varsayilanEsik);
     }
 }
